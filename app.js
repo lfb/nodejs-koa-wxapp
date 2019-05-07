5c38a24cd1c6fce7ba419c34fb05d5a6a59c17a7
liangfengbo <itbo@163.com>
2019-05-07 11:47:11 +0800

    初始化管理器和Process.cwd

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
 const Koa = require('koa')
-const Router = require('koa-router')
-const requireDirectory = require('require-directory')
+const InitManager = require('./core/init')
 
 const app = new Koa()
 
-// 路由自动加载
-requireDirectory(module, './api', {
-    visit: whenLoadModule
-})
-
-// 判断 requireDirectory 加载的模块是否为路由
-function whenLoadModule(obj) {
-    if (obj instanceof Router) {
-        app.use(obj.routes())
-    }
-}
+InitManager.initCore(app)
 
 app.listen(3000)
---
+++ core/init.js
@@ -0,0 +1,29 @@
+const Router = require('koa-router')
+const requireDirectory = require('require-directory')
+
+class InitManager {
+    static initCore(app) {
+        // 入口方法
+        InitManager.app = app;
+        InitManager.initLoadRouters()
+    }
+
+    // 加载全部路由
+    static initLoadRouters() {
+        // 绝对路径
+        const apiDirectory = `${process.cwd()}/app/api`
+        // 路由自动加载
+        requireDirectory(module, apiDirectory, {
+            visit: whenLoadModule
+        })
+
+        // 判断 requireDirectory 加载的模块是否为路由
+        function whenLoadModule(obj) {
+            if (obj instanceof Router) {
+                InitManager.app.use(obj.routes())
+            }
+        }
+    }
+}
+
+module.exports = InitManager
