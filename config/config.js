6b19d1bbc5fc0bea964b206502f8d2e97a20e7e2
liangfengbo <itbo@163.com>
2019-05-08 09:05:06 +0800

    sequelize初始化配置

--- config/config.js
+++ config/config.js
@@ -1,3 +1,10 @@
 module.exports = {
-    environment: 'dev'
+    environment: 'dev',
+    database: {
+        dbName: 'wxapp',
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'password'
+    }
 }
---
+++ core/db.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize')
+
+const {
+    dbName,
+    host,
+    port,
+    user,
+    password
+} = require('../config/config').database
+
+const sequelize = new Sequelize(dbName, user, password, {
+    dialect: 'mysql',
+    host,
+    port,
+    logging: true,
+    timezone: '+08:00',
+    define: {}
+})
+
+module.exports = {
+    sequelize
+}
